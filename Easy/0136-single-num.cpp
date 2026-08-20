080bc45f56f71672415a3a764897f07d6605e5fd
JosephDotio <yusuf041124@gmail.com>
2026-08-20 22:20:16 +0530

    Solve LeetCode #136 Single Number

---
+++ Easy/0136-single-num.cpp
@@ -0,0 +1,21 @@
+// Single Number :
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int singleNumber(vector <int> &nums) {
+    int ans = 0;
+
+    for (int val : nums) {
+        ans = ans ^ val;
+    }
+    return ans;
+}
+
+int main() {
+    vector <int> nums = {4, 1, 2, 1, 2};
+
+    cout << singleNumber(nums) << endl;
+
+    return 0;
+}
