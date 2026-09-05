af670c356836aa54f5b0a3443330c859d09b11c2
Md Yusuf <yusuf041124@gmail.com>
2026-09-05 20:38:41 +0530

    Solve LeetCode #876 Middle of the Linked List

---
+++ Easy/0876-middle-of-the-linked-list.cpp
@@ -0,0 +1,51 @@
+// >---- LeetCode : 876. Middle of the Linked List  ----->
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+*/ 
+
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution obj;
+
+    ListNode* middle = obj.middleNode(head);
+
+    cout << "Middle node: " << middle->val << endl;
+
+    return 0;
+}
